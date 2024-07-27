1d2b48a7c0ad8581b8ae82ec0179b3b97a81a250
nemesissanto <95344083+MajorMarcus@users.noreply.github.com>
2024-07-27 07:25:29 +0530

    asas
    asasa

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+from bs4 import BeautifulSoup
+import requests
+
+app = Flask(__name__)
+
+@app.route('/scrape', methods=['GET'])
+def scrape():
+    url = request.args.get('url')
+
+    if not url:
+        return jsonify({'error': 'URL is required'}), 400
+
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        news_items = []
+
+        for article in soup.find_all('article', class_='NewsTeaser_teaser__BR1_B'):
+            title = article.find('p', class_='NewsTeaser_teaser__title__OsMxr').text
+            subtitle = article.find('p', class_='NewsTeaser_teaser__preview__ZRFyi').text
+            img_tag = article.find('img', class_='ImageWithSets_of-image__img__pezo7 teaser__img')
+            img_url = img_tag['src'] if img_tag else None
+            
+            news_items.append({
+                'title': title,
+                'subtitle': subtitle,
+                'img_url': img_url
+            })
+
+        return jsonify(news_items)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
